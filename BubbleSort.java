2e8aa3b2e1b5578d7215b5267211f6996edc4084
Sagnik Adhikary <166514625+sagnikishere@users.noreply.github.com>
2025-01-05 23:54:58 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
