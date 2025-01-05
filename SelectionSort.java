c45aaca07ae05854086b51cd41348f1fe9be812a
Sagnik Adhikary <166514625+sagnikishere@users.noreply.github.com>
2025-01-05 23:56:41 +0530

    SelectionShort

---
+++ SelectionSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
