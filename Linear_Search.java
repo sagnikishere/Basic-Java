15cdf37f1355f956f65c5ea6ad68194eb1460e42
Sagnik Adhikary <166514625+sagnikishere@users.noreply.github.com>
2025-01-06 00:06:55 +0530

    searching algo

---
+++ Linear_Search.java
@@ -0,0 +1,21 @@
+public class Linear_Search {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return index if element is found
+            }
+        }
+        return -1; // Return -1 if not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+        int result = linearSearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
