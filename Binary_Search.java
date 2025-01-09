d884853e68d1e978abf2a7eaa9ce3cd0b45b1467
Sagnik Adhikary <166514625+sagnikishere@users.noreply.github.com>
2025-01-09 16:50:24 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -6,7 +6,7 @@ public class Binary_Search {
     public static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
-                return i; // Return index if element is found
+                return i; // Retu rn index if element is found
             }
         }
         return -1; // Return -1 if not found
